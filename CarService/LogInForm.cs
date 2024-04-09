c7a1c5640fb1d4a0b37650540cabc1cb28636e57
Ivan Topychkanov <ogurchikvk0907@gmail.com>
2024-04-10 00:19:49 +0500

    forms

---
+++ CarService/LogInForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarService
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void labelCreateAccount_Click(object sender, EventArgs e)
+        {
+            new RegForm().Show();
+            Hide();
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ CarService/RegForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace CarService
+{
+    public partial class RegForm : Form
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public RegForm()
+        {
+            InitializeComponent();
+            server = "localhost";
+            database = "carservice";
+            uid = "locAdmin";
+            password = "locAdmin";
+
+            string connectionString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};charset=utf8mb4";
+            connection = new MySqlConnection(connectionString);  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonReg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                string userLogin = textBoxLogin.Text;
+
+                string checkQuery = "SELECT COUNT(*) FROM users WHERE Username = @UserName";
+                MySqlCommand checkCmd = new MySqlCommand(checkQuery, connection);
+                checkCmd.Parameters.AddWithValue("@UserName", userLogin);
+                int userCount = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                if (userCount > 0)
+                {
+                    MessageBox.Show("Пользователь с таким именем уже существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    connection.Close();
+                    return;
+                }
+
+                string query = "INSERT INTO employees (FirstName, LastName, Position, Username, Password) VALUES (@FirstName, @LastName, @Position, @Username, @Password)";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@FirstName", textBoxFrstName.Text);
+                cmd.Parameters.AddWithValue("@LastName", textBoxLastName.Text);
+                cmd.Parameters.AddWithValue("@Position", comboBoxPosition.Text);
+                cmd.Parameters.AddWithValue("@Username", textBoxLogin.Text);
+                cmd.Parameters.AddWithValue("@Password", HashPassword(textBoxPassword.Text));
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при открытии соединения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+            }
+
+        }
+
+        private void labelLogIn_Click(object sender, EventArgs e)
+        {
+            new LogInForm().Show();
+            Hide();
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            return ByteArraysEqual(buffer3, buffer4);
+        }
+
+        public static bool ByteArraysEqual(byte[] b1, byte[] b2)
+        {
+            if (b1 == b2) return true;
+            if (b1 == null || b2 == null) return false;
+            if (b1.Length != b2.Length) return false;
+            for (int i = 0; i < b1.Length; i++)
+            {
+                if (b1[i] != b2[i]) return false;
+            }
+            return true;
+        }
+    }
+}
