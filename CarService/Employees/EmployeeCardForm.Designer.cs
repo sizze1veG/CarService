ba52f011963376317a9456e8eacf715eadb20d26
Topychkanov Ivan <ogurchikvk0907@gmail.com>
2024-05-23 03:55:24 +0500

    23.05.2024 3:55

--- CarService/Employees/EmployeeCardForm.Designer.cs
+++ CarService/Employees/EmployeeCardForm.Designer.cs
@@ -42,6 +42,10 @@
             this.comboBoxPosition = new System.Windows.Forms.ComboBox();
             this.labelUsername = new System.Windows.Forms.Label();
             this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelConfPassword = new System.Windows.Forms.Label();
+            this.textBoxConfPass = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
@@ -53,9 +57,10 @@
             this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDelete.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonDelete.ForeColor = System.Drawing.Color.White;
-            this.buttonDelete.Location = new System.Drawing.Point(344, 546);
+            this.buttonDelete.Location = new System.Drawing.Point(459, 672);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(225, 52);
+            this.buttonDelete.Size = new System.Drawing.Size(300, 64);
             this.buttonDelete.TabIndex = 47;
             this.buttonDelete.Text = "Удалить";
             this.buttonDelete.UseVisualStyleBackColor = false;
@@ -67,9 +72,10 @@
             this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonUpdate.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonUpdate.ForeColor = System.Drawing.Color.White;
-            this.buttonUpdate.Location = new System.Drawing.Point(46, 546);
+            this.buttonUpdate.Location = new System.Drawing.Point(61, 672);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(225, 52);
+            this.buttonUpdate.Size = new System.Drawing.Size(300, 64);
             this.buttonUpdate.TabIndex = 46;
             this.buttonUpdate.Text = "Обновить";
             this.buttonUpdate.UseVisualStyleBackColor = false;
@@ -81,9 +87,10 @@
             this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAdd.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonAdd.ForeColor = System.Drawing.Color.White;
-            this.buttonAdd.Location = new System.Drawing.Point(46, 546);
+            this.buttonAdd.Location = new System.Drawing.Point(61, 672);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(225, 52);
+            this.buttonAdd.Size = new System.Drawing.Size(300, 64);
             this.buttonAdd.TabIndex = 45;
             this.buttonAdd.Text = "Добавить";
             this.buttonAdd.UseVisualStyleBackColor = false;
@@ -91,20 +98,23 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(43, 39);
             this.pictureBox1.TabIndex = 44;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // textBoxLastName
             // 
             this.textBoxLastName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
             this.textBoxLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxLastName.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxLastName.Location = new System.Drawing.Point(417, 187);
+            this.textBoxLastName.Location = new System.Drawing.Point(556, 230);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxLastName.Name = "textBoxLastName";
-            this.textBoxLastName.Size = new System.Drawing.Size(363, 36);
+            this.textBoxLastName.Size = new System.Drawing.Size(484, 45);
             this.textBoxLastName.TabIndex = 38;
             // 
             // textBoxFirstName
@@ -112,18 +122,20 @@
             this.textBoxFirstName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
             this.textBoxFirstName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxFirstName.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxFirstName.Location = new System.Drawing.Point(417, 98);
+            this.textBoxFirstName.Location = new System.Drawing.Point(556, 121);
+            this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxFirstName.Name = "textBoxFirstName";
-            this.textBoxFirstName.Size = new System.Drawing.Size(363, 36);
+            this.textBoxFirstName.Size = new System.Drawing.Size(484, 45);
             this.textBoxFirstName.TabIndex = 37;
             // 
             // labelPosition
             // 
             this.labelPosition.AutoSize = true;
             this.labelPosition.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPosition.Location = new System.Drawing.Point(412, 233);
+            this.labelPosition.Location = new System.Drawing.Point(549, 297);
+            this.labelPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPosition.Name = "labelPosition";
-            this.labelPosition.Size = new System.Drawing.Size(152, 30);
+            this.labelPosition.Size = new System.Drawing.Size(183, 37);
             this.labelPosition.TabIndex = 41;
             this.labelPosition.Text = "Должность";
             // 
@@ -131,9 +143,10 @@
             // 
             this.labelLastName.AutoSize = true;
             this.labelLastName.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLastName.Location = new System.Drawing.Point(412, 144);
+            this.labelLastName.Location = new System.Drawing.Point(549, 177);
+            this.labelLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelLastName.Name = "labelLastName";
-            this.labelLastName.Size = new System.Drawing.Size(129, 30);
+            this.labelLastName.Size = new System.Drawing.Size(151, 37);
             this.labelLastName.TabIndex = 42;
             this.labelLastName.Text = "Фамилия";
             // 
@@ -141,18 +154,20 @@
             // 
             this.labelFirstName.AutoSize = true;
             this.labelFirstName.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelFirstName.Location = new System.Drawing.Point(412, 55);
+            this.labelFirstName.Location = new System.Drawing.Point(549, 68);
+            this.labelFirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelFirstName.Name = "labelFirstName";
-            this.labelFirstName.Size = new System.Drawing.Size(66, 30);
+            this.labelFirstName.Size = new System.Drawing.Size(81, 37);
             this.labelFirstName.TabIndex = 43;
             this.labelFirstName.Text = "Имя";
             // 
             // pictureBox
             // 
             this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
-            this.pictureBox.Location = new System.Drawing.Point(46, 48);
+            this.pictureBox.Location = new System.Drawing.Point(61, 59);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox.Size = new System.Drawing.Size(341, 315);
             this.pictureBox.TabIndex = 40;
             this.pictureBox.TabStop = false;
             // 
@@ -167,18 +182,20 @@
             "Менеджер",
             "Механик",
             "Бухгалтер"});
-            this.comboBoxPosition.Location = new System.Drawing.Point(417, 280);
+            this.comboBoxPosition.Location = new System.Drawing.Point(556, 345);
+            this.comboBoxPosition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxPosition.Name = "comboBoxPosition";
-            this.comboBoxPosition.Size = new System.Drawing.Size(363, 45);
+            this.comboBoxPosition.Size = new System.Drawing.Size(483, 54);
             this.comboBoxPosition.TabIndex = 48;
             // 
             // labelUsername
             // 
             this.labelUsername.AutoSize = true;
             this.labelUsername.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelUsername.Location = new System.Drawing.Point(412, 344);
+            this.labelUsername.Location = new System.Drawing.Point(1072, 68);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(88, 30);
+            this.labelUsername.Size = new System.Drawing.Size(103, 37);
             this.labelUsername.TabIndex = 42;
             this.labelUsername.Text = "Логин";
             // 
@@ -187,31 +204,81 @@
             this.textBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
             this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxUsername.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUsername.Location = new System.Drawing.Point(417, 387);
+            this.textBoxUsername.Location = new System.Drawing.Point(1079, 121);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(363, 36);
+            this.textBoxUsername.Size = new System.Drawing.Size(484, 45);
             this.textBoxUsername.TabIndex = 38;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(1072, 177);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(163, 46);
+            this.labelPassword.TabIndex = 42;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(1079, 230);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(484, 45);
+            this.textBoxPassword.TabIndex = 38;
+            // 
+            // labelConfPassword
+            // 
+            this.labelConfPassword.AutoSize = true;
+            this.labelConfPassword.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConfPassword.Location = new System.Drawing.Point(1072, 297);
+            this.labelConfPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelConfPassword.Name = "labelConfPassword";
+            this.labelConfPassword.Size = new System.Drawing.Size(163, 46);
+            this.labelConfPassword.TabIndex = 42;
+            this.labelConfPassword.Text = "Пароль";
+            // 
+            // textBoxConfPass
+            // 
+            this.textBoxConfPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textBoxConfPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxConfPass.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxConfPass.Location = new System.Drawing.Point(1079, 350);
+            this.textBoxConfPass.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxConfPass.Name = "textBoxConfPass";
+            this.textBoxConfPass.Size = new System.Drawing.Size(484, 45);
+            this.textBoxConfPass.TabIndex = 38;
             // 
             // EmployeeCardForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1124, 628);
+            this.ClientSize = new System.Drawing.Size(1638, 773);
             this.Controls.Add(this.comboBoxPosition);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxConfPass);
+            this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.textBoxLastName);
             this.Controls.Add(this.textBoxFirstName);
             this.Controls.Add(this.labelPosition);
+            this.Controls.Add(this.labelConfPassword);
+            this.Controls.Add(this.labelPassword);
             this.Controls.Add(this.labelUsername);
             this.Controls.Add(this.labelLastName);
             this.Controls.Add(this.labelFirstName);
             this.Controls.Add(this.pictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "EmployeeCardForm";
             this.Text = "EmployeeCardForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -236,5 +303,9 @@
         private System.Windows.Forms.ComboBox comboBoxPosition;
         private System.Windows.Forms.Label labelUsername;
         private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelConfPassword;
+        private System.Windows.Forms.TextBox textBoxConfPass;
     }
 }
--- CarService/Employees/EmployeeCardForm.cs
+++ CarService/Employees/EmployeeCardForm.cs
@@ -18,6 +18,124 @@ namespace CarService.Employees
         public EmployeeCardForm(bool isNew, bool isReadOnly, string id = "-1")
         {
             InitializeComponent();
+            connection = new DBConnection().GetConnectionString();
+
+            if (isReadOnly)
+            {
+                textBoxFirstName.Enabled = false;
+                textBoxLastName.Enabled = false;
+                comboBoxPosition.Enabled = false;
+                textBoxUsername.Enabled = false;
+                textBoxPassword.Visible = false;
+                textBoxConfPass.Visible = false;
+            }
+            if (isNew)
+            {
+                buttonAdd.Visible = true;
+                buttonAdd.Enabled = true;
+                buttonDelete.Visible = false;
+                buttonDelete.Enabled = false;
+                buttonUpdate.Visible = false;
+                textBoxPassword.Visible = true;
+                textBoxConfPass.Visible = true;
+            }
+            else
+            {
+                ID = Convert.ToInt32(id);
+                buttonAdd.Visible = false;
+                buttonAdd.Enabled = false;
+                if (isReadOnly)
+                {
+                    buttonAdd.Visible = false;
+                    buttonDelete.Visible = false;
+                    buttonUpdate.Visible = false;
+                    textBoxPassword.Visible = false;
+                    textBoxConfPass.Visible = false;
+                }
+                else
+                {
+                    buttonDelete.Visible = true;
+                    buttonDelete.Enabled = true;
+                    buttonUpdate.Visible = true;
+                    textBoxPassword.Visible = true;
+                    textBoxConfPass.Visible = true;
+                }
+
+                try
+                {
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+                    string query = @"SELECT Employees.ID, Employees.FirstName, Employees.LastName, Employees.Position, 
+                        Employees.Username
+                        FROM Employees 
+                        WHERE Employees.ID = @EmployeeID LIMIT 1";
+
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@EmployeeID", ID);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            textBoxFirstName.Text = reader.GetString("FirstName");
+                            textBoxLastName.Text = reader.GetString("LastName");
+                            if (reader.GetString("Position") == "Administrator")
+                            {
+                                comboBoxPosition.Enabled = false;
+                            }
+                            comboBoxPosition.SelectedItem = reader.GetString("Position");
+                            textBoxUsername.Text = reader.GetString("Username");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private bool CheckInput()
+        {
+            if (textBoxFirstName.Text.Length == 0)
+            {
+                MessageBox.Show("Неверно введена марка.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+                textBoxBrand.Focus();
+                return false;
+            }
+            if (textBoxModel.Text.Length == 0)
+            {
+                MessageBox.Show("Неверно введена модель.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+                textBoxModel.Focus();
+                return false;
+            }
+            if (Convert.ToInt32(textBoxYear.Text) < 1900 || Convert.ToInt32(textBoxYear.Text) > DateTime.Now.Year)
+            {
+                MessageBox.Show("Неверно введен год.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+                textBoxYear.Focus();
+                return false;
+            }
+            if (comboBoxClient.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите клиента.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+                comboBoxClient.Focus();
+                return false;
+            }
+            return true;
         }
     }
 }
